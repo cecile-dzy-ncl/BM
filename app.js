5d7181fd0a7e0f9025dc833ef795429b57abc736
Cecile Dezy <cecile.dezy@gmail.com>
2019-11-11 16:33:53 +0100

    reussir son projet

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 $( document ).ready(function() {
   if (location.pathname != '/') {
-      var page = location.pathname.split('/')[2];
+      // var page = location.pathname.split('/')[2]; // à remettre une fois qu'on passe sur iraiser
+      var page = location.pathname.split('.')[0].split("/"); //à retirer une fois qu'on passe sur iraiser
+      page = page[page.length-1]; //à retirer une fois qu'on passe sur iraiser
       console.log(page);
       $('body').addClass(page);
     } else {
