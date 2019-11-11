cfa0e10fbf461e1fd2c1473e741d55be5b1330c1
Cecile Dezy <cecile.dezy@gmail.com>
2019-11-11 16:20:23 +0100

    reussir son projet

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 $( document ).ready(function() {
   if (location.pathname != '/') {
       var page = location.pathname.split('/')[2];
+      console.log(page);
       $('body').addClass(page);
     } else {
    $('body').addClass('homepage');
