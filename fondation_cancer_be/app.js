aee2d39fea933cb32dab76e57ba0eeac6f12c618
Cecile Dezy <cecile.dezy@gmail.com>
2019-11-23 09:48:57 +0100

    js

--- fondation_cancer_be/app.js
+++ fondation_cancer_be/app.js
@@ -10,7 +10,7 @@ $( document ).ready(function() {
 
 
   // AJOUT INTRO PAGE "CRÉER SA COLLECTE"
-  var textIntroEvents = ("<div class='container-small-bm mt-30'><h2>Deux minutes suffisent pour créer votre collecte.</h2><p>Choisissez dans quelle collecte vous souhaitez vous inscrire (ci-dessous), choisissez un titre, une photo et ça y est, vous êtes prête à commencer votre récolte de fonds pour financer la lutte contre le cancer. Nous vous avons même préparé des <a href='https://idonatefor.cancer.be/pages/conseils-outils' class='green'>conseils et outils pour vous aider à réussir votre projet.</a></p></div>")
+  var textIntroEvents = ("<div class='container-small-bm mt-50'><h2>Deux minutes suffisent pour créer votre collecte.</h2><p>Choisissez dans quelle collecte vous souhaitez vous inscrire (ci-dessous), choisissez un titre, une photo et ça y est, vous êtes prête à commencer votre récolte de fonds pour financer la lutte contre le cancer. Nous vous avons même préparé des <a href='https://idonatefor.cancer.be/pages/conseils-outils' class='green'>conseils et outils pour vous aider à réussir votre projet.</a></p></div>")
   $('.events .first-section').before(textIntroEvents);
 
 
