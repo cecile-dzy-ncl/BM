af0f0d99e954a04e85e8416f44c7514f1f70b92e
Cecile Dezy <cecile.dezy@gmail.com>
2019-11-29 10:23:25 +0100

    integration

--- viva_for_life/app.js
+++ viva_for_life/app.js
@@ -1,4 +1,10 @@
 $( document ).ready(function() {
   console.log( "ready!" );
   $("body").addClass("vfl");
+
+
+  // AJOUT INTRO PAGE EVENTS
+  var textIntroEvents = ("<div class='container-small-bm mt-50'><h2>Deux minutes suffisent pour créer votre collecte.</h2><p>Choisissez dans quelle collecte vous souhaitez vous inscrire (ci-dessous), choisissez un titre, une photo et ça y est, vous êtes prête à commencer votre récolte de fonds pour financer la lutte contre le cancer. Nous vous avons même préparé des <a href='https://idonatefor.cancer.be/pages/conseils-outils' class='green'>conseils et outils pour vous aider à réussir votre projet.</a></p></div>")
+  $('.events .first-section').before(textIntroEvents);
+
 });
