2a060764dc8126fc8ea64f205a2887ecf126ee3b
Cecile Dezy <cecile.dezy@gmail.com>
2019-12-01 22:52:56 +0100

    1001 fontaines

--- 1001_fontaines/app.js
+++ 1001_fontaines/app.js
@@ -7,7 +7,7 @@ $( document ).ready(function() {
   $(".infos-money .big").after("collectés");
 
   // AJOUT TITRE "COLLECTES EN COURS" EN BAS DE PAGE
-  $('.section-home-projects').prepend('<h2>Exemples de collectes en cours</h2>');
+  $('.section-home-projects').prepend('<h2>Exemples de campagnes en cours</h2>');
 
   // AJOUT INTRO PAGE EVENTS
   var textIntroEvents = ("<div class='container-small-bm mt-50'><h2>Deux minutes suffisent pour créer votre collecte.</h2><p>Choisissez dans quelle collecte vous souhaitez vous inscrire (ci-dessous), choisissez un titre, une photo et ça y est, vous êtes prête à commencer votre récolte de fonds pour financer la lutte contre le cancer. Nous vous avons même préparé des <a href='https://idonatefor.cancer.be/pages/conseils-outils' class='green'>conseils et outils pour vous aider à réussir votre projet.</a></p></div>")
