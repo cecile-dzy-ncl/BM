c749f8b05b565a760c56573fc2468abfd39b72d8
Cecile Dezy <cecile.dezy@gmail.com>
2019-12-01 12:28:22 +0100

    integration

--- fondation_cancer_be/app.js
+++ fondation_cancer_be/app.js
@@ -5,26 +5,43 @@ $( document ).ready(function() {
 
   $(".infos-money .big").after("collectés");
 
-  // AJOUT TITRE "COLLECTES EN COURS" EN BAS DE PAGE
-  $('.section-home-projects').prepend('<h2>Exemples de collectes en cours</h2>');
 
-
-  // AJOUT INTRO PAGE EVENTS
-  var textIntroEvents = ("<div class='container-small-bm mt-50'><h2>Deux minutes suffisent pour créer votre collecte.</h2><p>Choisissez dans quelle collecte vous souhaitez vous inscrire (ci-dessous), choisissez un titre, une photo et ça y est, vous êtes prête à commencer votre récolte de fonds pour financer la lutte contre le cancer. Nous vous avons même préparé des <a href='https://idonatefor.cancer.be/pages/conseils-outils' class='green'>conseils et outils pour vous aider à réussir votre projet.</a></p></div>")
-  $('.events .first-section').before(textIntroEvents);
-
-  // AJOUT INTRO PAGE PROJETS
-  var textIntroProjects = ("<div class='container-small-bm mt-50'><h2>Retrouver et soutenir une collecte</h2></div>")
-  $('.projects .first-section').before(textIntroProjects);
-
-  // AJOUT BANNER MAINTENANCE
 
   if ($("html").prop("lang") === ("fr")) {
+
+    // AJOUT TITRE "COLLECTES EN COURS" EN BAS DE PAGE
+    $('.section-home-projects').prepend('<h2>Exemples de collectes en cours</h2>');
+
+
+    // AJOUT INTRO PAGE EVENTS
+    var textIntroEvents = ("<div class='container-small-bm mt-50'><h2>Deux minutes suffisent pour créer votre collecte.</h2><p>Choisissez dans quelle collecte vous souhaitez vous inscrire (ci-dessous), choisissez un titre, une photo et ça y est, vous êtes prête à commencer votre récolte de fonds pour financer la lutte contre le cancer. Nous vous avons même préparé des <a href='https://idonatefor.cancer.be/pages/conseils-outils' class='green'>conseils et outils pour vous aider à réussir votre projet.</a></p></div>")
+    $('.events .first-section').before(textIntroEvents);
+
+    // AJOUT INTRO PAGE PROJETS
+    var textIntroProjects = ("<div class='container-small-bm mt-50'><h2>Retrouver et soutenir une collecte</h2></div>")
+    $('.projects .first-section').before(textIntroProjects);
+
+    // AJOUT BANNER MAINTENANCE
     var bannerMaintenance = ("<div class='maintenance p-5 bg-green'><p class='m-0 white'>Notre plateforme est en cours d’actualisation, nous nous excusons pour le dérangement.</p></div>");
     $("#banner_section").before(bannerMaintenance);
+
   } else if ($("html").prop("lang") === ("nl")) {
+
+    // AJOUT TITRE "COLLECTES EN COURS" EN BAS DE PAGE
+    $('.section-home-projects').prepend('<h2>Voorbeelden</h2>');
+
+    // AJOUT INTRO PAGE EVENTS
+    var textIntroEvents = ("<div class='container-small-bm mt-50'><h2>Deux minutes suffisent pour créer votre collecte.</h2><p>Choisissez dans quelle collecte vous souhaitez vous inscrire (ci-dessous), choisissez un titre, une photo et ça y est, vous êtes prête à commencer votre récolte de fonds pour financer la lutte contre le cancer. Nous vous avons même préparé des <a href='https://idonatefor.cancer.be/pages/conseils-outils' class='green'>conseils et outils pour vous aider à réussir votre projet.</a></p></div>")
+    $('.events .first-section').before(textIntroEvents);
+
+    // AJOUT INTRO PAGE PROJETS
+    var textIntroProjects = ("<div class='container-small-bm mt-50'><h2>Retrouver et soutenir une collecte</h2></div>")
+    $('.projects .first-section').before(textIntroProjects);
+
+    // AJOUT BANNER MAINTENANCE
     var bannerMaintenanceNL = ("<div class='maintenance p-5 bg-green'><p class='m-0 white'>We zijn bezig met een update van ons platform, gelieve ons te verontschuldigen voor eventuele ongemakken</p></div>");
     $("#banner_section").before(bannerMaintenanceNL);
+
   }
 
 });
