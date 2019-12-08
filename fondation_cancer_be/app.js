c5a4cc0fe91d9501b599ff77f777535f98080537
Cecile Dezy <cecile.dezy@gmail.com>
2019-12-08 13:06:21 +0100

    responsive

--- fondation_cancer_be/app.js
+++ fondation_cancer_be/app.js
@@ -45,14 +45,10 @@ $( document ).ready(function() {
 
   // --------- RESPONSIVE ---------
 
-  // var windowWidth= $(window).width();
+  var windowWidth= $(window).width();
 
-  // if(windowWidth < 500){
-  //   $("body").addClass("mobile");
-  //   $(".steps .position-absolute").removeClass("position-absolute");
-  //   $(".mobile .h-360").removeClass("h-360");
-  //   $(".mobile img.mln-30").removeClass("mln-30");
-  //   $(".mobile .bx-viewport").css('height', "auto");
-  // }
+  if(windowWidth < 500){
+    $(".cards-bm .mb-21").removeClass(".mb-21");
+  }
 
 });
