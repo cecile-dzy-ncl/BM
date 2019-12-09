814a6136f473a4a4cdba42591f82ed38d56b430b
Cecile Dezy <cecile.dezy@gmail.com>
2019-12-09 13:15:07 +0100

    responsive

--- viva_for_life/app.js
+++ viva_for_life/app.js
@@ -3,10 +3,6 @@ $( document ).ready(function() {
   $("body").addClass("vfl");
 
 
-  // AJOUT INTRO PAGE EVENTS
-  var textIntroEvents = ("<div class='container-small-bm mt-50'><h2>Rejoignez dès maintenant un défi collectif RTBF et commencez à collecter des fonds pour les enfants.</h2><p>Que vous soyez plutôt sportif, danseur ou comédien dans l’âme, vous trouverez forcément un défi qui vous ressemble pour venir en aide aux enfants victimes de la pauvreté.</p><p class='font-bold'>Une question sur les défis collectifs ?</br>Parlons-en au 02/737.44.52 ou par email : <a href='mailto:napu@rtbf.be' class='orange'>napu@rtbf.be</a></p></div>")
-  $('.events .first-section').before(textIntroEvents);
-
   // AJOUT TITRE "COLLECTES EN COURS" EN BAS DE PAGE
   $('.section-home-projects').prepend('<h2 class="mt-60 mb-50">Exemples de défis en cours</h2>');
 
@@ -25,7 +21,8 @@ var idCampaign = $("body").prop("classList")[1].split("_")[1];
 
 
 //ajout du bloc en début de page
-$('#events-show .habillage').prepend('<div class="stats-event mobile_flex-column"><div class="stats-event-1"><img src="https://agir.vivaforlife.be/cdn.iraiser.eu/6idKBeqjdjVcRtnL6A9qeBCllCI4cS9vXnl62IzT33g2GL6oGHWGvWlLOZdR4Xs7/Cecile_DEZY/origin/iconpeople2x.png" id="img-stat" alt=""><div class="stats-event-nb"><p>Déjà <span class="" style="font-size: 32px"><tag event_id='+ idCampaign +'></tag></span> collecteurs engagés</p></div></div><div class="line"></div><div class="stats-event-2"><div class="stats-event-montants mobile_flex-column"><div class="stats-event-m-1"><span>97 560 €</span> collectés</div><div class="stats-event-m-2">Objectif: <span>100 000 €</span></div></div><div class="progress"><span class="graph-barBack"><span class="graph-bar" data-value="10"></span></span></div></div></div></div>');
+// $('#events-show .habillage').prepend('<div class="stats-event mobile_flex-column"><div class="stats-event-1"><img src="https://agir.vivaforlife.be/cdn.iraiser.eu/6idKBeqjdjVcRtnL6A9qeBCllCI4cS9vXnl62IzT33g2GL6oGHWGvWlLOZdR4Xs7/Cecile_DEZY/origin/iconpeople2x.png" id="img-stat" alt=""><div class="stats-event-nb"><p>Déjà <span class="" style="font-size: 32px"><tag event_id='+ idCampaign +'></tag></span> collecteurs engagés</p></div></div><div class="line"></div><div class="stats-event-2"><div class="stats-event-montants mobile_flex-column"><div class="stats-event-m-1"><span>97 560 €</span> collectés</div><div class="stats-event-m-2">Objectif: <span>100 000 €</span></div></div><div class="progress"><span class="graph-barBack"><span class="graph-bar" data-value="10"></span></span></div></div></div></div>');
+$('#events-show .habillage').prepend('<div class="stats-event mobile_flex-column"><div class="stats-event-2"><div class="stats-event-montants mobile_flex-column"><div class="stats-event-m-1"><span>97 560 €</span> collectés</div><div class="stats-event-m-2">Objectif: <span>100 000 €</span></div></div><div class="progress"><span class="graph-barBack"><span class="graph-bar" data-value="10"></span></span></div></div></div></div>');
 
   // barre de progression
   var pourcentage = $('.chart').attr('data-percent');
