7191492a521c2a7db61e1d050beaf6b97e4befa1
Cecile Dezy <cecile.dezy@gmail.com>
2019-12-16 15:38:56 +0100

    responsive

--- 1001_fontaines/app.js
+++ 1001_fontaines/app.js
@@ -15,6 +15,7 @@ $( document ).ready(function() {
     console.log(nombreEnfants);
     $('.nb_enfants span').html(nombreEnfants);
 
+    console.log($(".nb_ecoles span").html().split('€'));
     console.log(parseInt($(".nb_ecoles span").html().split('€')[0])/10);
     var montantEcoleRegion = Math.floor(parseInt($(".nb_ecoles span").html().split('€')[0]) / 1000);
     console.log(montantEcoleRegion);
