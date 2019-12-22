2407738b3eef1e6ba897d4351a467113dbd2c4d7
Cecile Dezy <cecile.dezy@gmail.com>
2019-12-22 19:55:35 +0100

    calculs

--- 1001_fontaines/app.js
+++ 1001_fontaines/app.js
@@ -13,7 +13,7 @@ $( document ).ready(function() {
 
     var nombreEnfants = Math.floor(parseInt($(".nb_enfants span").html().split('€')[0]) / 2);
     console.log(Math.floor(parseInt($(".nb_enfants span").html().split('€'))));
-    console.log(Math.floor(parseInt($(".nb_enfants span").html().split('€')[0]));
+    console.log(Math.floor(parseInt($(".nb_enfants span").html().split('€')[0])));
     console.log(nombreEnfants);
     $('.nb_enfants span').html(nombreEnfants);
 
