3862f1d69323c3b422d81fdfab10676755a8248f
Cecile Dezy <cecile.dezy@gmail.com>
2020-02-10 14:11:11 +0100

    arc

---
+++ arc/app.js
@@ -0,0 +1,23 @@
+$( document ).ready(function() {
+  console.log( "ready!" );
+
+  $("body").addClass("arc");
+
+  // $(".project_boutons_give").html("Faire un don");
+
+  // AJOUT TITRE "COLLECTES EN COURS" EN BAS DE PAGE
+  // $('.section-home-projects').prepend('<h2>Exemples de campagnes en cours</h2>');
+
+  // AJOUT INTRO PAGE EVENTS
+  // var textIntroEvents = ("<div class='container-small-bm mt-50'><h2>Deux minutes suffisent pour créer votre collecte.</h2></div>")
+  // $('.events .first-section').before(textIntroEvents);
+
+  // AJOUT INTRO PAGE PROJETS
+  // var textIntroProjects = ("<div class='container-small-bm mt-50'><h2>Retrouver et soutenir une collecte</h2></div>")
+  // $('.projects .first-section').before(textIntroProjects);
+
+  // AJOUT BANNER MAINTENANCE
+  // var bannerMaintenance = ("<div class='maintenance p-5 bg-blue'><p class='m-0 white'>Notre plateforme est en cours d’actualisation, nous nous excusons pour le dérangement.</p></div>");
+  // $("#banner_section").before(bannerMaintenance);
+
+});
