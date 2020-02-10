f3757b6b7b1cb9abc7f78e8c259511a1c298df4b
Cecile Dezy <cecile.dezy@gmail.com>
2020-02-10 14:36:47 +0100

    arc

--- arc/app.js
+++ arc/app.js
@@ -17,7 +17,7 @@ $( document ).ready(function() {
   // $('.projects .first-section').before(textIntroProjects);
 
   // AJOUT BANNER MAINTENANCE
-  var bannerMaintenance = ("<div class='maintenance p-5 bg-blue'><p class='m-0 white'>Notre plateforme est en cours d’actualisation, nous nous excusons pour le dérangement.</p></div>");
+  var bannerMaintenance = ("<div class='maintenance text-center p-5 bg-blue'><p class='m-0 white'>Notre plateforme est en cours d’actualisation, nous nous excusons pour le dérangement.</p></div>");
   $("#banner_section").before(bannerMaintenance);
 
 });
