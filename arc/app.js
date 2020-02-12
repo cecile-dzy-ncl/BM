41fca5faea3d486de5c92d1c00263c2a93894067
Cecile Dezy <cecile.dezy@gmail.com>
2020-02-12 12:53:37 +0100

    arc

--- arc/app.js
+++ arc/app.js
@@ -13,8 +13,8 @@ $( document ).ready(function() {
   $('.section-home-projects').prepend('<h2 class="text-center mb-40 black">Exemples de campagnes en cours</h2>');
 
   // AJOUT INTRO PAGE EVENTS
-  // var textIntroEvents = ("<div class='container-small-bm mt-50'><h2>Deux minutes suffisent pour créer votre collecte.</h2></div>")
-  // $('.events .first-section').before(textIntroEvents);
+  var textIntroEvents = ("<div class='container-small-bm mt-50'><h2>Deux minutes suffisent pour créer votre collecte.</h2></div>")
+  $('.events .first-section').before(textIntroEvents);
 
   // AJOUT INTRO PAGE PROJETS
   // var textIntroProjects = ("<div class='container-small-bm mt-50'><h2>Retrouver et soutenir une collecte</h2></div>")
@@ -25,3 +25,8 @@ $( document ).ready(function() {
   $("#banner_section").before(bannerMaintenance);
 
 });
+
+
+Choisissez ci-dessous dans quelle opération vous souhaitez vous inscrire, choisissez un titre pour votre projet, une photo et ça y’est, votre page de collecte est prête, vous pouvez commencer à collecter des dons au proft de la Fondation ARC pour la recherche sur le cancer.
+
+Une question ? N’hésitez pas à nous contacter et à profiter de nos conseils et outils.
