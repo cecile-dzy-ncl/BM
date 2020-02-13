01c5e44f7e7d2483c85c06d48fe7fb007cee1a80
Cecile Dezy <cecile.dezy@gmail.com>
2020-02-13 16:06:04 +0100

    arc

--- arc/app.js
+++ arc/app.js
@@ -75,8 +75,10 @@ $( document ).ready(function() {
         dataType: "html",
         success: function (data) {
           console.log(data);
+
           var wrapper = data.find($(".wrapper-help"));
           console.log(wrapper);
+          console.log(wrapper.type());
             // var result = $('<div />').append(data).find('#showresults').html();
             // $('#showresults').html(result);
         }
