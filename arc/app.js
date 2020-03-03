efb1c70e3a255d9124ff8d23726667e7aedb3217
Cecile Dezy <cecile.dezy@gmail.com>
2020-03-03 19:30:09 +0100

    last fixes

--- arc/app.js
+++ arc/app.js
@@ -37,7 +37,7 @@ $( document ).ready(function() {
     <h2 class="white text-center"><span class="font-300">BESOIN D'AIDE ? </span>NOS CONSEILS &amp; OUTILS</h2>
     <p class="white text-center">Parce que nous voulons que votre expérience soit une réussite, nous vous avons préparé une série de conseils et d’outils pour mener à bien votre projet : guide de collecte, goodies, vidéos, cover Facebook, etc.</p>
     <div class="btn-bm border w-50 my-30"><a class="white" href="https://collecter.fondation-arc.org/pages/conseils">DÉCOUVRIR NOS CONSEILS &amp; OUTILS</a></div>
-    <p class="white">Vous pouvez également<a class="underline white" href="" target="blank"> nous contacter par email </a>ou nous téléphoner au 01 56 98 85 85.</p>
+    <p class="white">Vous pouvez également<a class="underline white" href="https://collecter.fondation-arc.org/contact_forms/new" target="blank"> nous contacter par email </a>ou nous téléphoner au 01 56 98 85 85.</p>
     </div></div>`);
   $("#events-index .section-events").after(wrapperOutils);
 
