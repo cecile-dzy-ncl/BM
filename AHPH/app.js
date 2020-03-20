4910336860c7d60ab4bba3b537331857129da521
Cecile Dezy <cecile.dezy@gmail.com>
2020-03-20 17:32:48 -0500

    AHPH

--- AHPH/app.js
+++ AHPH/app.js
@@ -66,8 +66,8 @@ $( document ).ready(function() {
   if(windowWidth < 500){
     $("body").addClass("mobile");
      $(".pr-370").removeClass("pr-370");
-    // $(".banner-text h2").removeClass("white");
-    // $(".banner-text p").removeClass("white");
+    $(".banner-text h2").removeClass("white");
+    $(".banner-text p").removeClass("white");
     // $(".steps img").removeClass("ml-60");
     // $(".steps").removeClass("pt-150").removeClass("pb-100");
     // $(".a-propos .steps").removeClass("w-50");
