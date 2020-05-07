476cb182e04208a7ae9a26086d86feb8bb156636
Cecile Dezy <cecile.dezy@gmail.com>
2020-05-07 19:41:21 +0200

    emmaus

--- emmaus/app.js
+++ emmaus/app.js
@@ -21,8 +21,8 @@ $( document ).ready(function() {
   });
 
   // AJOUT BANNER MAINTENANCE
-  var bannerMaintenance = ("<div class='maintenance p-5 bg-blue'><p class='m-0 white text-center'>Notre plateforme est en cours d’actualisation, nous nous excusons pour le dérangement.</p></div>");
-  $("#banner_section").before(bannerMaintenance);
+  // var bannerMaintenance = ("<div class='maintenance p-5 bg-blue'><p class='m-0 white text-center'>Notre plateforme est en cours d’actualisation, nous nous excusons pour le dérangement.</p></div>");
+  // $("#banner_section").before(bannerMaintenance);
 
 
 
