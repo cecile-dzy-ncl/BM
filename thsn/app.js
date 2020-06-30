ff0a935776d1648643e314f52d8cba6bea3260c6
Cecile Dezy <cecile.dezy@gmail.com>
2020-06-30 23:29:29 +0200

    front main event

--- thsn/app.js
+++ thsn/app.js
@@ -66,6 +66,7 @@ $( document ).ready(function() {
 
   var shareDiv =  `<div class="d-flex justify-center align-items-center w-100">\
   <input type="textarea" class='url-link' id="input" placeholder="${pageUrl}" value="${pageUrl}"> \
+  <textarea class='url-link' id="input" placeholder="${pageUrl}" value="${pageUrl}">${pageUrl}</textarea> \
   <div class='url-btn' id="copy">COPIER LIEN</div> \
   </div>`;
 
