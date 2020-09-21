b98596ed35800430d876a81f26d0c9d68e26914c
Cecile Dezy <cecile.dezy@gmail.com>
2020-09-21 21:49:21 +0200

    flex grow

--- abbe_pierre/app.js
+++ abbe_pierre/app.js
@@ -77,8 +77,8 @@ $( document ).ready(function() {
           var projectCard = `
           <div class='project-card'>
             <img src='${projectImg}' alt='photo du projet'>
-            <div class='project-details d-flex flex-column p-50'>
-              <h4 class='text-center flex-grow-1'>${projectTitle}</h4>
+            <div class='project-details d-flex flex-column p-50 h-50'>
+              <h4 class='flex-grow-1 d-flex align-items-center justify-center'>${projectTitle}</h4>
               <div>
                 <div class='d-flex'>
                   <div class='w-33'>
