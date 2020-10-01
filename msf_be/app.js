c6dafe34e7ed9960b3f69b76e2498e8d642b12e7
Cecile Dezy <cecile.dezy@gmail.com>
2020-10-01 11:56:15 +0200

    msf

--- msf_be/app.js
+++ msf_be/app.js
@@ -1,1 +1,6 @@
-console.log("coucou");
+console.log("hey hey");
+
+// AJOUT BANNER MAINTENANCE
+var bannerMaintenance =
+  "<div class='maintenance p-5 bg-black'><p class='m-0 white text-center'>Notre plateforme est en cours d’actualisation, nous nous excusons pour le dérangement.</p></div>";
+$("#banner_section").before(bannerMaintenance);
