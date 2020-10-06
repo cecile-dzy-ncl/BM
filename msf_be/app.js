10030ad73607c61bdd518cc53f78b5dc18def433
Cecile Dezy <cecile.dezy@gmail.com>
2020-10-06 22:39:32 +0200

    msf

--- msf_be/app.js
+++ msf_be/app.js
@@ -90,7 +90,8 @@ $(document).ready(function () {
 
   if ($(".panel")) {
     console.log($(".panel"));
-    // ("linear-gradient(137deg, rgba(255,44,0,1) 0%, rgba(255,44,0,1) 50%, rgba(125,3,0,0.8561799719887955) 50%, rgba(0,0,0,0) 55%);");
+    $(".panel").style.background =
+      "linear-gradient(137deg, rgba(255,44,0,1) 0%, rgba(255,44,0,1) 50%, rgba(125,3,0,0.8561799719887955) 50%, rgba(0,0,0,0) 55%);";
   }
 
   var navButtons = $(".header-spacer nav[role='navigation']").html();
