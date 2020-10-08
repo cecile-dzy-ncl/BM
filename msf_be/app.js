0e746d18b8545766d46c23e05851b795f7f2f789
Cecile Dezy <cecile.dezy@gmail.com>
2020-10-08 15:52:40 +0200

    msf

--- msf_be/app.js
+++ msf_be/app.js
@@ -19,6 +19,31 @@ $(document).ready(function () {
 
   $("body").addClass("msf-be");
 
+  var navButtons = $(".header-spacer nav[role='navigation']").html();
+  // console.log(navButtons);
+  $("#header-header").before(
+    `<section id='first-header'><div class='row'></div></section>`
+  );
+
+  $("#first-header > div").html(navButtons);
+
+  $("#header-header > .row > .columns:last-child").after(
+    `<div class="btn-actions">
+    <a class="bg-grey" href="https://events.msf-azg.be/projects"><strong class="white">Soutenir
+    </strong>une collecte</a>
+    <a class="bg-red" href=""><strong class="white">Créer
+    </strong>une collecte</a>
+    </div>`
+  );
+
+  $("#header-header > .row > .columns").removeClass(
+    "small-12 medium-12 large-4 large-8 columns"
+  );
+
+  $(".header-spacer > div").removeClass(
+    "small-12 medium-push-6 medium-pull-6 medium-6 large-reset-order large-6 columns"
+  );
+
   // ENLEVER LES DÉCIMALES ET AJOUTER LE SIGLE €
   // $("#tag-amount").html($("#tag-amount").text().split(",")[0]);
   // $('#tag-amount').html($('#tag-amount').text().split(",")[0] + " €")
@@ -98,31 +123,6 @@ $(document).ready(function () {
   //     );
   //   });
   // }
-
-  var navButtons = $(".header-spacer nav[role='navigation']").html();
-  // console.log(navButtons);
-  $("#header-header").before(
-    `<section id='first-header'><div class='row'></div></section>`
-  );
-
-  $("#first-header > div").html(navButtons);
-
-  $("#header-header > .row > .columns:last-child").after(
-    `<div class="btn-actions">
-    <a class="bg-grey" href="https://events.msf-azg.be/projects"><strong class="white">Soutenir
-    </strong>une collecte</a>
-    <a class="bg-red" href=""><strong class="white">Créer
-    </strong>une collecte</a>
-    </div>`
-  );
-
-  $("#header-header > .row > .columns").removeClass(
-    "small-12 medium-12 large-4 large-8 columns"
-  );
-
-  $(".header-spacer > div").removeClass(
-    "small-12 medium-push-6 medium-pull-6 medium-6 large-reset-order large-6 columns"
-  );
 
   // EVENT STARS S'ENGAGENT
   $(function () {
