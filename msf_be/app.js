096afccb7e1657b2926afb97fa68516fb08c9b1d
Cecile Dezy <cecile.dezy@gmail.com>
2020-10-11 02:31:17 +0200

    msf be

--- msf_be/app.js
+++ msf_be/app.js
@@ -58,7 +58,7 @@ $(document).ready(function () {
     if ($("body").is("#events-show")) {
       // ajout du bloc stats en début de page
       $("section.event-head > .row > div:last-child").prepend(
-        "<div class='event-recap'>prout</div>"
+        "<div class='event-recap'></div>"
       );
       // $("#events-show .habillage").prepend(
       //   '<div class="event-stats"><div class="event-stat projects_count border-right"><div class="stat-nb"><img src="https://cagnottes.fondation-abbe-pierre.fr/cdn.iraiser.eu/I39oA4rMH9qdybsOpBgq0iwzVt7FMRAsvHp9T9pmvz+9lRcfTzKeuu6wlx2lJCDc/Cecile_Dezy/origin/iconpagecollecte2x.png"><span class="orange"></span></div><p class="text-center mb-0"><strong>page de collecte créées</strong></p></div><div class="event-stat amount_collected"><div class="stat-nb"><img src="https://cagnottes.fondation-abbe-pierre.fr/cdn.iraiser.eu/I39oA4rMH9qdybsOpBgq0iwzVt7FMRAsvHp9T9pmvz+9lRcfTzKeuu6wlx2lJCDc/Cecile_Dezy/origin/iconmontantcollecte2x.png"><span class="orange"></span></div><p class="text-center mb-0"><strong>ont déjà été collectés</strong></p></div></div>'
