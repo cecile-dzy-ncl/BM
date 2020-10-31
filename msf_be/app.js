d8071027cab85212aa3c431a1bdaf4600479165e
Cecile Dezy <cecile.dezy@gmail.com>
2020-11-01 00:38:45 +0100

    msf be

--- msf_be/app.js
+++ msf_be/app.js
@@ -1,12 +1,36 @@
-import updateNavbar from "./navbar";
-
 $(document).ready(function () {
   $("body").addClass("msf-be");
-  updateNavbar();
+
   // AJOUT BANNER MAINTENANCE
   var bannerMaintenance =
     "<div class='maintenance p-5 bg-black'><p class='m-0 white text-center'>Notre plateforme est en cours d’actualisation, nous nous excusons pour le dérangement.</p></div>";
   $("#banner_section").before(bannerMaintenance);
+
+  // AJOUT NAV BUTTONS
+  var navButtons = $(".header-spacer nav[role='navigation']").html();
+  // console.log(navButtons);
+  $("#header-header").before(
+    `<section id='first-header'><div class='row'></div></section>`
+  );
+
+  $("#first-header > div").html(navButtons);
+
+  $("#header-header > .row > .columns:last-child").after(
+    `<div class="btn-actions">
+    <a class="bg-grey" href="https://events.msf-azg.be/projects"><strong class="white">Soutenir
+    </strong>une collecte</a>
+    <a class="bg-red" href=""><strong class="white">Créer
+    </strong>une collecte</a>
+    </div>`
+  );
+
+  $("#header-header > .row > .columns").removeClass(
+    "small-12 medium-12 large-4 large-8 columns"
+  );
+
+  $(".header-spacer > div").removeClass(
+    "small-12 medium-push-6 medium-pull-6 medium-6 large-reset-order large-6 columns"
+  );
 
   $(".section-my-event > .row > .small-12.medium-9.columns")
     .removeClass("medium-9")
