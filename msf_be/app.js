6437729ccfa61e6a8df63d1d362e5463d8bcc8ee
Cecile Dezy <cecile.dezy@gmail.com>
2020-11-15 19:49:52 +0100

    msf

--- msf_be/app.js
+++ msf_be/app.js
@@ -389,7 +389,11 @@ $(document).ready(function () {
         // injectCard(value, "#urgences-scroll .scrolling-wrapper");
       });
 
-      if (collectesEvents.length > 3) {
+      if (
+        collectesEvents.length > 3 ||
+        events.length > 3 ||
+        urgencesEvents.length > 3
+      ) {
         console.log(collectesEvents);
       }
     }
