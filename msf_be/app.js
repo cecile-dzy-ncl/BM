e0b4f955ceed810b6731793f847d4410891d3a39
Cecile Dezy <cecile.dezy@gmail.com>
2020-11-22 19:29:54 +0100

    msf

--- msf_be/app.js
+++ msf_be/app.js
@@ -438,6 +438,7 @@ $(document).ready(function () {
 
   if ($("#projects-new")) {
     $(".tab-content").prepend($("#step_tab"));
+    $("#step_tab").after($("#error-message + .row"));
     //   $("#projects-new .container-event").before(
     //     "<h1 class='step-title text-center'></h1>"
     //   );
