c450e36ba3f2e05bf802e658a3dc39c2c5cef308
Cecile Dezy <cecile.dezy@gmail.com>
2020-11-22 19:15:10 +0100

    msf

--- msf_be/app.js
+++ msf_be/app.js
@@ -436,62 +436,62 @@ $(document).ready(function () {
   //   $(".step-title").html(activeStepTitleContent);
   // }
 
-  // if ($("#projects-new")) {
-  //   $("form#new_project").before($("#step_tab"));
-  //   $("#projects-new .container-event").before(
-  //     "<h1 class='step-title text-center'></h1>"
-  //   );
-  //   var activeStepTitle = $(
-  //     "#projects-new #form-project #step_tab .active a.click_tabs"
-  //   )[0];
+  if ($("#projects-new")) {
+    $("form#new_project").prepend($("#step_tab"));
+    //   $("#projects-new .container-event").before(
+    //     "<h1 class='step-title text-center'></h1>"
+    //   );
+    //   var activeStepTitle = $(
+    //     "#projects-new #form-project #step_tab .active a.click_tabs"
+    //   )[0];
 
-  //   // if (activeStepTitle) {
-  //   //   // var activeStepTitleContent = activeStepTitle.innerHTML.split("-")[1];
-  //   //   // $(".step-title").html(activeStepTitleContent);
-  //   //   changeTitle(activeStepTitle);
-  //   // }
-  //   // $("input#step").change(function () {
-  //   //   var activeStepTitle = $(
-  //   //     "#projects-new #form-project #step_tab .active a.click_tabs"
-  //   //   )[0];
-  //   //   changeTitle(activeStepTitle);
-  //   //   var stepNumber = $("input#step")[0].value;
-  //   //   console.log(stepNumber);
-  //   // });
+    //   // if (activeStepTitle) {
+    //   //   // var activeStepTitleContent = activeStepTitle.innerHTML.split("-")[1];
+    //   //   // $(".step-title").html(activeStepTitleContent);
+    //   //   changeTitle(activeStepTitle);
+    //   // }
+    //   // $("input#step").change(function () {
+    //   //   var activeStepTitle = $(
+    //   //     "#projects-new #form-project #step_tab .active a.click_tabs"
+    //   //   )[0];
+    //   //   changeTitle(activeStepTitle);
+    //   //   var stepNumber = $("input#step")[0].value;
+    //   //   console.log(stepNumber);
+    //   // });
 
-  //   $("form#new_project").change(function () {
-  //     //   var activeStepTitle = $(
-  //     //     "#projects-new #form-project #step_tab .active a.click_tabs"
-  //     //   )[0];
-  //     //   changeTitle(activeStepTitle);
-  //     //   var stepNumber = $("input#step")[0].value;
-  //     //   console.log("if form", stepNumber);
-  //     //   console.log("if form activeStepTitle", activeStepTitle);
+    //   $("form#new_project").change(function () {
+    //     //   var activeStepTitle = $(
+    //     //     "#projects-new #form-project #step_tab .active a.click_tabs"
+    //     //   )[0];
+    //     //   changeTitle(activeStepTitle);
+    //     //   var stepNumber = $("input#step")[0].value;
+    //     //   console.log("if form", stepNumber);
+    //     //   console.log("if form activeStepTitle", activeStepTitle);
 
-  //     if ($("input#step")[0].value === "1") {
-  //       console.log("if 1", activeStepTitle);
-  //       // var activeStepTitle = $(
-  //       //   "#projects-new #form-project #step_tab .active a.click_tabs"
-  //       // )[0];
-  //       // changeTitle(activeStepTitle);
-  //     }
+    //     if ($("input#step")[0].value === "1") {
+    //       console.log("if 1", activeStepTitle);
+    //       // var activeStepTitle = $(
+    //       //   "#projects-new #form-project #step_tab .active a.click_tabs"
+    //       // )[0];
+    //       // changeTitle(activeStepTitle);
+    //     }
 
-  //     if ($("input#step")[0].value === "2") {
-  //       // var activeStepTitle = $(
-  //       //   "#projects-new #form-project #step_tab .active a.click_tabs"
-  //       // )[0];
-  //       // changeTitle(activeStepTitle);
-  //       console.log("if 2", activeStepTitle);
-  //     }
+    //     if ($("input#step")[0].value === "2") {
+    //       // var activeStepTitle = $(
+    //       //   "#projects-new #form-project #step_tab .active a.click_tabs"
+    //       // )[0];
+    //       // changeTitle(activeStepTitle);
+    //       console.log("if 2", activeStepTitle);
+    //     }
 
-  //     if ($("input#step")[0].value === "3") {
-  //       // var activeStepTitle = $(
-  //       //   "#projects-new #form-project #step_tab .active a.click_tabs"
-  //       // )[0];
-  //       // changeTitle(activeStepTitle);
-  //       console.log("if 3", activeStepTitle);
-  //     }
-  //   });
+    //     if ($("input#step")[0].value === "3") {
+    //       // var activeStepTitle = $(
+    //       //   "#projects-new #form-project #step_tab .active a.click_tabs"
+    //       // )[0];
+    //       // changeTitle(activeStepTitle);
+    //       console.log("if 3", activeStepTitle);
+    //     }
+    //   });
   }
 
   // --------- RESPONSIVE ---------
