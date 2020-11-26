72527365c5584f3c01347075b1f86ea22bc3b93c
Cecile Dezy <cecile.dezy@gmail.com>
2020-11-26 23:40:02 +0100

    events msf

--- msf_be/app.js
+++ msf_be/app.js
@@ -414,9 +414,9 @@ $(document).ready(function () {
           `https://events.msf-azg.be/api/events?api_id=7b22e2a84173efacQFVJQBOHUISTHNNI&api_secret=b0d584cb2bba825cdaa6104c503883c8`,
           function (response) {
             console.log(response.events);
-            $.each(response, function (key, value) {
+            $.each(response.events, function (key, value) {
               console.log(value);
-              // buildCard(value, eventContainer);
+              buildCard(value, eventContainer);
             });
           }
         );
