96bca634fdffdb7e94d9eea49b973bedfbbed234
Cecile Dezy <cecile.dezy@gmail.com>
2020-12-07 00:55:03 +0100

    action enfance

--- msf_be/app.js
+++ msf_be/app.js
@@ -414,14 +414,14 @@ $(document).ready(function () {
           '<button id="next" class="controls"><i class="fas fa-arrow-right"></i></button>',
         responsive: [
           {
-            breakpoint: 600,
+            breakpoint: 768,
             settings: {
               slidesToShow: 2,
               slidesToScroll: 2,
             },
           },
           {
-            breakpoint: 480,
+            breakpoint: 500,
             settings: {
               slidesToShow: 1,
               slidesToScroll: 1,
