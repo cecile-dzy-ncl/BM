c43cfb4a9d04b878d194eaee7d22aa22729d6889
Cecile Dezy <cecile.dezy@gmail.com>
2020-12-17 00:46:17 +0100

    msf

--- msf_be/app.js
+++ msf_be/app.js
@@ -485,15 +485,19 @@ $(document).ready(function () {
         }
       }
 
-      // if ($("body").is("#events-index")) {
-      //   const eventsContainer = $("#events-index #events");
-      //   eventsContainer.html("");
-      //   var eventsList = [4, 5, 8, 9, 10, 11, 85, 86, 88];
+      if ($("body").is("#events-index")) {
+        $.each($("#events .panel .percent"), function (key, value) {
+          console.log(value);
+          console.log($(this));
+        });
 
-      //   $.each(eventsList, function (key, value) {
-      //     fetchData(value, eventsContainer);
-      //   });
-      // }
+        //   const eventsContainer = $("#events-index #events");
+        //   eventsContainer.html("");
+        //   var eventsList = [4, 5, 8, 9, 10, 11, 85, 86, 88];
+        //   $.each(eventsList, function (key, value) {
+        //     fetchData(value, eventsContainer);
+        //   });
+      }
       // fetchDataEvents(eventsContainer);
     }
   });
