a7a5036292228b3de06918383a6670e61b467014
Cecile Dezy <cecile.dezy@gmail.com>
2020-12-17 01:53:33 +0100

    msf

--- msf_be/app.js
+++ msf_be/app.js
@@ -498,9 +498,9 @@ $(document).ready(function () {
 
           console.log($(this).closest(".panel .wrap .row"));
           $(this).closest(".panel .wrap .row").append(progressBarEvent);
-          $(".event-item .infos-money").prepend(
-            $(this).closest(".event-item .event-description .infos")
-          );
+          $(this)
+            .closest(".event-item .infos-money")
+            .prepend($(this).closest(".event-item .event-description .infos"));
         });
       }
     }
