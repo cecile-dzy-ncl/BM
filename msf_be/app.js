6afa5d848521722ad61b64c8720f5bd1c3cac0de
Cecile Dezy <cecile.dezy@gmail.com>
2020-12-29 00:26:04 +0100

    msf be

--- msf_be/app.js
+++ msf_be/app.js
@@ -296,10 +296,10 @@ $(document).ready(function () {
           currency: "EUR",
         }).format(eventAmountCollected);
 
-        if (response.expected_amount != null) {
+        if (event.expected_amount != null) {
           console.log("ugh");
           var eventAmountExpected =
-            Number.parseInt(response.expected_amount, 10) / 100;
+            Number.parseInt(event.expected_amount, 10) / 100;
           var eventProgress =
             (eventAmountCollected / eventAmountExpected) * 100;
           var progressBar = `
