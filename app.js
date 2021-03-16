c70be5a57897b82144088360a58bc2c3631e9bed
Cecile Dezy <cecile.dezy@gmail.com>
2021-03-16 13:18:21 +0100

    fix(action enfance): add active class on navigation

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
-$( document ).ready(function() {
-  if (location.pathname != '/') {
-      // var page = location.pathname.split('/')[2]; // à remettre une fois qu'on passe sur iraiser
-      var page = location.pathname.split('.')[0].split("/"); //à retirer une fois qu'on passe sur iraiser
-      page = page[page.length-1]; //à retirer une fois qu'on passe sur iraiser
-      console.log(page);
-      $('body').addClass(page);
-    } else {
-   $('body').addClass('homepage');
+$(document).ready(function () {
+  if (location.pathname != "/") {
+    // var page = location.pathname.split('/')[2]; // à remettre une fois qu'on passe sur iraiser
+    var page = location.pathname.split(".")[0].split("/"); //à retirer une fois qu'on passe sur iraiser
+    page = page[page.length - 1]; //à retirer une fois qu'on passe sur iraiser
+    console.log(page);
+    $("body").addClass(page);
+  } else {
+    $("body").addClass("homepage");
   }
 
+  $(function () {
+    if (location.pathname != "/") {
+      $('a[href^="' + location.pathname + '"]').addClass("active");
+    } else {
+      $('a[href="/"]').addClass("active");
+    }
+  });
+
   // $("footer ul.right img").attr("src", "https://agir.vivaforlife.be/cdn.iraiser.eu/6idKBeqjdjVcRtnL6A9qeBCllCI4cS9vXnl62IzT33g2GL6oGHWGvWlLOZdR4Xs7/Cecile_DEZY/origin/poweredbyiraiser2x.png")
-  $("footer ul.right img").attr("src", "/cdn.iraiser.eu/6idKBeqjdjVcRtnL6A9qeBCllCI4cS9vXnl62IzT33g2GL6oGHWGvWlLOZdR4Xs7/Cecile_DEZY/origin/poweredbyiraiser2x.png")
+  $("footer ul.right img").attr(
+    "src",
+    "/cdn.iraiser.eu/6idKBeqjdjVcRtnL6A9qeBCllCI4cS9vXnl62IzT33g2GL6oGHWGvWlLOZdR4Xs7/Cecile_DEZY/origin/poweredbyiraiser2x.png"
+  );
 
-// --------- RESPONSIVE ---------
+  // --------- RESPONSIVE ---------
 
-  var windowWidth= $(window).width();
+  var windowWidth = $(window).width();
 
-  if(windowWidth < 500){
+  if (windowWidth < 500) {
     $("body").addClass("mobile");
     $(".phone").removeClass("position-absolute");
     $(".mobile .h-360").removeClass("h-360");
@@ -24,11 +35,6 @@ $( document ).ready(function() {
     $(".mobile .projet-head .user").removeClass(".right");
     $(".mobile .projet-head .team_by").removeClass(".left");
     // $(".mobile img.mln-30").removeClass("mln-30");
-    $(".mobile .bx-viewport").css('height', "auto");
+    $(".mobile .bx-viewport").css("height", "auto");
   }
-
-
-
-
-
 });
