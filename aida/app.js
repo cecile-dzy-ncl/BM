f38b58aecb11e42dc3cffb91f9287aa5bea66408
Cecile Dezy <cecile.dezy@gmail.com>
2021-04-15 18:53:43 +0200

    feat(aida): style

--- aida/app.js
+++ aida/app.js
@@ -33,6 +33,18 @@ $(document).ready(function () {
       </div>`;
   $(".events .first-section").before(textIntroEvents);
 
+  // AJOUT WRAPPER OUTILS
+  var wrapperOutils = `<!-- BLOC OUTILS -->
+ <div class="wrapper wrapper-help bg-darkblue">
+ <div class="container-small-bm">
+ <img alt="" class="d-block mx-auto mb-30" src="https://collecter.fondation-arc.org/cdn.iraiser.eu/8v+/iZhRxdeu2BVNVycHEZ06vL2iKxpZId225sg6zFv3invHFgijE79b/k15iwJG/Simon_Romain/origin/arc-icondiscuss2x.png" style="height: 50px;">
+ <h2 class="white text-center"><span class="font-300">BESOIN D'AIDE ? </span>NOS CONSEILS &amp; OUTILS</h2>
+ <p class="white text-center">Parce que nous voulons que votre expérience soit une réussite, nous vous avons préparé une série de conseils et d’outils pour mener à bien votre projet : guide de collecte, goodies, vidéos, cover Facebook, etc.</p>
+ <div class="btn-bm border w-50 my-30"><a class="white" href="https://collecter.fondation-arc.org/pages/conseils">DÉCOUVRIR NOS CONSEILS &amp; OUTILS</a></div>
+ <p class="white">Vous pouvez également<a class="underline white" href="https://collecter.fondation-arc.org/contact_forms/new" target="blank"> nous contacter par email </a>ou nous téléphoner au 01 56 98 85 85.</p>
+ </div></div>`;
+  $("#events-index .section-events").after(wrapperOutils);
+
   // --------- RESPONSIVE ---------
 
   // var windowWidth = $(window).width();
