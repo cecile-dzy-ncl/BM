4fb42c5e01839970782abd37f5a127dc1fd7f298
Cecile Dezy <cecile.dezy@gmail.com>
2021-04-15 18:57:02 +0200

    feat(aida): style

--- aida/app.js
+++ aida/app.js
@@ -38,10 +38,9 @@ $(document).ready(function () {
  <div class="wrapper wrapper-help bg-darkblue">
  <div class="container-small-bm">
  <img alt="" class="d-block mx-auto mb-30" src="https://collecter.fondation-arc.org/cdn.iraiser.eu/8v+/iZhRxdeu2BVNVycHEZ06vL2iKxpZId225sg6zFv3invHFgijE79b/k15iwJG/Simon_Romain/origin/arc-icondiscuss2x.png" style="height: 50px;">
- <h2 class="white text-center"><span class="font-300">BESOIN D'AIDE ? </span>NOS CONSEILS &amp; OUTILS</h2>
- <p class="white text-center">Parce que nous voulons que votre expérience soit une réussite, nous vous avons préparé une série de conseils et d’outils pour mener à bien votre projet : guide de collecte, goodies, vidéos, cover Facebook, etc.</p>
- <div class="btn-bm border w-50 my-30"><a class="white" href="https://collecter.fondation-arc.org/pages/conseils">DÉCOUVRIR NOS CONSEILS &amp; OUTILS</a></div>
- <p class="white">Vous pouvez également<a class="underline white" href="https://collecter.fondation-arc.org/contact_forms/new" target="blank"> nous contacter par email </a>ou nous téléphoner au 01 56 98 85 85.</p>
+ <h2 class="white text-center"><span class="font-300">BESOIN D'AIDE ? </span>Contactez-nous !</h2>
+ <a class="white text-center" href="mailto:allezleschampions@associationaida.org">allezleschampions@associationaida.org</a>
+ <p class="white text-center">Vous pouvez également nous contacter par téléphone au 06 32 71 00 10</p>
  </div></div>`;
   $("#events-index .section-events").after(wrapperOutils);
 
