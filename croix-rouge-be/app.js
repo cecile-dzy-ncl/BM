b0da9e2963411e782550d1d625a74faa891fbb0d
Cecile Dezy <cecile.dezy@gmail.com>
2021-05-18 00:54:13 +0200

    feat(CR BE): homepage

--- croix-rouge-be/app.js
+++ croix-rouge-be/app.js
@@ -42,11 +42,17 @@ $(document).ready(function () {
   }
 
   const holesEvents = [9, 10, 11];
+  const holesPodiumEvents = [];
 
-  const fetchData = function (event) {
+  const buildEventCard = (event) => {
+    var eventAmountCollected = parseInt(event.amount_collected, 10) / 100;
+  };
+
+  const fetchData = (event) => {
     var url = `https://help.redcross.be/api/events/${event}?api_id=d1e5432ae7ad6e34WDIDLZYKXTKQUKAD&api_secret=a35d14f0b5371808e6c19236cf7ec870`;
     $.get(url, function (response) {
       console.log(response);
+      console.log(response.amount_collected);
     });
   };
   if ($("body").is(".event_7.main-event")) {
