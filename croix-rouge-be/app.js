20be62089e104ef77a9a1059e0bd7342265ef60c
Cecile Dezy <cecile.dezy@gmail.com>
2021-05-17 22:07:08 +0200

    feat(CR BE): setup

---
+++ croix-rouge-be/app.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+  $("body").addClass("hole-heroes");
+
+  // ENLEVER LES DÉCIMALES ET AJOUTER LE SIGLE €
+  // $("#tag-amount").html($("#tag-amount").text().split(",")[0]);
+  // $('#tag-amount').html($('#tag-amount').text().split(",")[0] + " €")
+
+  $(
+    "<li class='nodropdown footer-link-menu'><a href='https://associationaida.org/' target='_blank'>© Association Aïda</a></li>"
+  ).prependTo("footer .top-bar-section ul.left ul.center");
+
+  // AJOUT BANNER MAINTENANCE
+  var bannerMaintenance =
+    "<div class='maintenance p-5 bg-black'><p class='m-0 white text-center'>Notre plateforme est en cours d’actualisation, nous nous excusons pour le dérangement.</p></div>";
+  $("#banner_section").before(bannerMaintenance);
+
+  // $("#events .event-item:first-of-type").before(challengeInf);
+
+  // --------- RESPONSIVE ---------
+
+  // var windowWidth = $(window).width();
+
+  // if (windowWidth < 500) {
+  //   $("body").addClass("mobile");
+  //   $(".pl-370").removeClass("pl-370");
+  //   $(".banner-text h2").removeClass("white").addClass("black");
+  //   $(".banner-text p").removeClass("white");
+  //   $(".campagne > div").removeClass("w-50");
+  //   $(".compteur .border-right")
+  //     .removeClass("border-right")
+  //     .removeClass("border-white");
+  //   $(".conseils-et-outils .bg-black .container-small-bm > .d-flex").remove();
+  //   $(".a-propos .wrapper .w-20").addClass("mb-15").removeClass("w-20");
+  //   $(".a-propos .wrapper .w-30").addClass("mb-15").removeClass("w-30");
+  //   $("#events-show .event-stat").removeClass("border-right");
+  //   $("#indexs-index .compteur .border-right").removeClass("border-right");
+  // }
+});
