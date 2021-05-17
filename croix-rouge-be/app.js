0e0646e2ea2a4d8c7d4b3e91a579dcb05f390998
Cecile Dezy <cecile.dezy@gmail.com>
2021-05-18 01:05:23 +0200

    feat(CR BE): homepage

--- croix-rouge-be/app.js
+++ croix-rouge-be/app.js
@@ -56,11 +56,10 @@ $(document).ready(function () {
       console.log(response.amount_collected);
       holesEventsList.push(response);
       holesEventsList.sort((a, b) =>
-        a.parseInt(event.amount_collected, 10) >
-        b.parseInt(event.amount_collected, 10)
+        a.parseInt(a.amount_collected, 10) > b.parseInt(b.amount_collected, 10)
           ? 1
-          : a.parseInt(event.amount_collected, 10) ===
-            b.parseInt(event.amount_collected, 10)
+          : a.parseInt(a.amount_collected, 10) ===
+            b.parseInt(b.amount_collected, 10)
           ? a.id > b.id
             ? 1
             : -1
