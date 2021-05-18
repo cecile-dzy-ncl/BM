df73fccfbccb018f0b5353d4b2466aca1a4be104
Cecile Dezy <cecile.dezy@gmail.com>
2021-05-19 00:47:56 +0200

    feat(CR BE): api

--- croix-rouge-be/app.js
+++ croix-rouge-be/app.js
@@ -53,7 +53,7 @@ $(document).ready(function () {
     var eventAmountCollected = parseInt(event.amount_collected, 10) / 100;
     console.log("event", event);
     const eventCard = `
-    <div class="eventCard bg-grey">
+    <div class="eventCard">
     <span>#${index + 1}</span>
     <img src="/" alt="club profile picture/>
     <div></div>
