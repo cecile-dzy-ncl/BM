57ffa90683f77abf9715a4b070dac828e00ca768
Cecile Dezy <cecile.dezy@gmail.com>
2021-05-24 23:26:24 +0200

    feat(CR BE): api

--- croix-rouge-be/app.js
+++ croix-rouge-be/app.js
@@ -155,9 +155,8 @@ const fetchSubEvents = (ids) => {
   const eventsList = [];
 
   ids.forEach(async (id) => {
-    let response = await fetch(
-      `https://help.redcross.be/api/events/${id}?api_id=d1e5432ae7ad6e34WDIDLZYKXTKQUKAD&api_secret=a35d14f0b5371808e6c19236cf7ec870`
-    );
+    // prettier-ignore
+    let response = await fetch(`https://help.redcross.be/api/events/${id}?api_id=d1e5432ae7ad6e34WDIDLZYKXTKQUKAD&api_secret=a35d14f0b5371808e6c19236cf7ec870`);
     event = await response.json();
     eventsList.push(event);
   });
