c657017f22181a935a2a4e5dc299e0c8748c5e78
Cecile Dezy <cecile.dezy@gmail.com>
2021-05-25 00:22:04 +0200

    feat(CR BE): api

--- croix-rouge-be/app.js
+++ croix-rouge-be/app.js
@@ -87,7 +87,7 @@ const buildPlayerCard = async (index, player, eventsList) => {
   <div class="player_RankImg">
   <span><span>#</span>${index + 1}</span>
   <img src="${
-    player.avatar
+    player.users_of_team[0].avatar
       ? player.users_of_team[0].avatar
       : "https://help.redcross.be/cdn.iraiser.eu/ch/vdrzIM224J80PqBVfikjruyXvm+tWBQ7A0+NbdBKBS+g3U4N+XSKsN8JAO/Ig/Marie-Dominique_Remion/avatar/CRBHolesforHeroes-pictogolfeur.png"
   }" alt="club profile picture"/>
