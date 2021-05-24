e2abeb380c1db465264199842e122b3d36f8c881
Cecile Dezy <cecile.dezy@gmail.com>
2021-05-24 22:20:34 +0200

    feat(CR BE): api

--- croix-rouge-be/app.js
+++ croix-rouge-be/app.js
@@ -142,31 +142,22 @@ const fetchSubEvent = (event) => {
 };
 
 const fetchSubEvents = (ids) => {
+  console.log("fetchSubevents");
   console.log(typeof ids);
+
   const eventsList = [];
   ids.forEach(async (id) => {
-    // console.log(id);
     let event = await fetch(
       `https://help.redcross.be/api/events/${id}?api_id=d1e5432ae7ad6e34WDIDLZYKXTKQUKAD&api_secret=a35d14f0b5371808e6c19236cf7ec870`
     );
     eventsList.push(event);
-    // eventsList.sort((a, b) =>
-    //   a.amount_collected > b.amount_collected
-    //     ? 1
-    //     : a.amount_collected === b.amount_collected
-    //     ? a.id > b.id
-    //       ? 1
-    //       : -1
-    //     : -1
-    // );
-    // $.each(eventsList, function (key, value) {
-    //   buildEventCard(key, value);
-    // });
   });
+
   console.log(eventsList);
 };
 
 const fetchMainEvent = () => {
+  console.log("fetchMainEvent");
   var url = `https://help.redcross.be/api/events/7?api_id=d1e5432ae7ad6e34WDIDLZYKXTKQUKAD&api_secret=a35d14f0b5371808e6c19236cf7ec870`;
   $.get(url, function (response) {
     fetchSubEvents(response.subevents);
@@ -179,3 +170,16 @@ if ($("body").is(".event_7.main-event")) {
   fetchMainEvent();
   fetchBestPlayers();
 }
+
+// eventsList.sort((a, b) =>
+//   a.amount_collected > b.amount_collected
+//     ? 1
+//     : a.amount_collected === b.amount_collected
+//     ? a.id > b.id
+//       ? 1
+//       : -1
+//     : -1
+// );
+// $.each(eventsList, function (key, value) {
+//   buildEventCard(key, value);
+// });
