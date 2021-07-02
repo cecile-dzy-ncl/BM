1f8868f4a7bb3261437215b42cc97188b4503bf7
Cecile Dezy <cecile.dezy@gmail.com>
2021-07-02 21:04:19 +0200

    feat(CR BE): corrections

--- croix-rouge-be/app.js
+++ croix-rouge-be/app.js
@@ -178,6 +178,8 @@ const fetchSubEvents = (ids) => {
 const fetchMainEvent = () => {
   var url = `https://help.redcross.be/api/events/7?api_id=d1e5432ae7ad6e34WDIDLZYKXTKQUKAD&api_secret=a35d14f0b5371808e6c19236cf7ec870`;
   $.get(url, function (response) {
+    console.log(response.subevents);
+    console.log(response.subevents.slice(0, 10));
     fetchSubEvents(response.subevents);
   });
 };
