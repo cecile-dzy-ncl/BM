8f7340bdc68a37f3709a3658ca600f48ae3496ff
Cecile Dezy <cecile.dezy@gmail.com>
2021-07-06 09:18:50 +0200

    fix(ADS): club nb

--- croix-rouge-be/app.js
+++ croix-rouge-be/app.js
@@ -177,6 +177,7 @@ const fetchSubEvents = (ids) => {
 
 const getClubsNb = (subevents) => {
   var clubNb = subevents.length;
+  console.log('clubNb', clubNb);
   document.querySelectorAll("club_nb").forEach((nb) => {
     nb.innerText = clubNb;
   })
