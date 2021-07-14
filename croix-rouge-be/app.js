c944072d28c72a05cbe2ac90a14f2095d82460d4
Cecile Dezy <cecile.dezy@gmail.com>
2021-07-14 12:26:23 +0200

    cr be

--- croix-rouge-be/app.js
+++ croix-rouge-be/app.js
@@ -185,6 +185,7 @@ const getClubsNb = (subevents) => {
 const fetchMainEvent = () => {
   var url = `https://help.redcross.be/api/events/7?api_id=d1e5432ae7ad6e34WDIDLZYKXTKQUKAD&api_secret=a35d14f0b5371808e6c19236cf7ec870`;
   $.get(url, function (response) {
+    console.log("response", response);
     fetchSubEvents(response.subevents.slice(0, 10));
     getClubsNb(response.subevents);
   });
