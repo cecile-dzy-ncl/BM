06fa9ad8e94a3992805147a37c225113b94c1d43
Cecile Dezy <cecile.dezy@gmail.com>
2021-08-08 19:24:17 +0200

    fix(CR BE): clubevents/clubs order

--- croix-rouge-be/app.js
+++ croix-rouge-be/app.js
@@ -149,7 +149,7 @@ const buildEventCard = (event, index) => {
 const buildEvents = (events) => {
   console.log("events", events);
   console.log(
-    "events sorted",
+    "events sorted 2",
     events.sort((a, b) => (b.amount_collected > a.amount_collected ? 1 : -1))
   );
   events
