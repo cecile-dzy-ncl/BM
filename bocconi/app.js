74de106436c6e54c6d065d4d90d2d6ece8de141a
Cecile Dezy <cecile.dezy@gmail.com>
2021-10-18 19:47:11 +0200

    feat(bocconi): style

--- bocconi/app.js
+++ bocconi/app.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     "<div class='maintenance p-5 bg-black'><p class='m-0 white text-center'>Our platform is currently being updated, we apologise for the inconvenience.</p></div>";
   $("#banner_section").before(bannerMaintenance);
 
-  // $("nav#main-nav .top-bar-section").prepend($(".front-logo"));
+  $("nav#main-nav .top-bar-section").prepend($(".front-logo"));
   // $("nav#main-nav .top-bar-section ul.center").after($(".top-bar.left"));
   // $("nav#main-nav > .top-bar-section").append($(".navbar-search"));
 
