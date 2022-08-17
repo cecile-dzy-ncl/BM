7a08dd3c0ec0a9bea5b5acc923eea4105444259f
ceciledezy <cecile.dezy@gmail.com>
2022-08-17 21:33:02 +0200

    CR FR

--- croix-rouge/app.js
+++ croix-rouge/app.js
@@ -42,7 +42,7 @@ $(document).ready(function () {
 
   $(function () {
     if ($("body").is(".projets-locaux")) {
-      const fetchUlEvents = (event) => {
+      const fetchUlEvents = () => {
         $.get(
           `https://macollecte.croix-rouge.fr/api/events?api_id=995dcc6271d03903LODRUVKNHNDGRMXF&api_secret=e519ac404340b6fc322cf90dcf6d9d91&count=300`,
           function (response) {
