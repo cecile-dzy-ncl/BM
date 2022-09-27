0c5f4bdad23020be8b0ece20dbdc22b537343a7c
ceciledezy <cecile.dezy@gmail.com>
2022-09-27 08:39:59 +0200

    feat(): SC

---
+++ secours-catholique/app.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+  $("body").addClass("secours-catholique");
+
+  // ENLEVER LES DÉCIMALES ET AJOUTER LE SIGLE €
+  // $("#tag-amount").html($("#tag-amount").text().split(",")[0]);
+  // $('#tag-amount').html($('#tag-amount').text().split(",")[0] + " €")
+
+  $(
+    "<li class='nodropdown footer-link-menu'><a href='http://www.secours-catholique.org/' target='_blank'>© Secours Catholique</a></li>"
+  ).prependTo("footer .top-bar-section ul.left ul.center");
+
+  // AJOUT BANNER MAINTENANCE
+  var bannerMaintenance =
+    "<div class='maintenance p-5 bg-black'><p class='m-0 white text-center'>Notre plateforme est en cours d’actualisation, nous nous excusons pour le dérangement.</p></div>";
+  $("#banner_section").before(bannerMaintenance);
+
+  $(function () {
+    if ($("body").is("#events-show")) {
+      var eventId = $("#widget-new-project a")[0].href.split("event_id=")[1];
+
+      var amount = $("#events-show .current-amount").html().replace(/\s/g, "");
+
+      var compteurDiv = `<div class="wrapper pt-0">
+        <div class="container-bm">
+        <div class="compteur">
+        <div class="w-50 px-10 mx-auto">
+        <h2 class="text-center  m-0"><span class="bitter red">${amount}</span></h2>
+        <p class="text-center  m-0">ont déjà été collectés</p>
+        </div>
+        </div>
+        </div>
+        </div>`;
+      $("#widget-new-project").before(compteurDiv);
+    }
+  });
+});
