d7e3af9385a3863095516e13a8747fc9cb188e4e
ceciledezy <cecile.dezy@gmail.com>
2023-01-20 13:12:52 +0100

    feat(): enlever le compteur cap48

--- viva_for_life/app.js
+++ viva_for_life/app.js
@@ -19,20 +19,18 @@ $(document).ready(function () {
   // recuperer id
   var idCampaign = $("body").prop("classList")[1].split("_")[1];
 
-  //ajout du bloc en début de page
-  // $('#events-show .habillage').prepend('<div class="stats-event mobile_flex-column"><div class="stats-event-1"><img src="https://agir.vivaforlife.be/cdn.iraiser.eu/6idKBeqjdjVcRtnL6A9qeBCllCI4cS9vXnl62IzT33g2GL6oGHWGvWlLOZdR4Xs7/Cecile_DEZY/origin/iconpeople2x.png" id="img-stat" alt=""><div class="stats-event-nb"><p>Déjà <span class="" style="font-size: 32px"><tag event_id='+ idCampaign +'></tag></span> collecteurs engagés</p></div></div><div class="line"></div><div class="stats-event-2"><div class="stats-event-montants mobile_flex-column"><div class="stats-event-m-1"><span>97 560 €</span> collectés</div><div class="stats-event-m-2">Objectif: <span>100 000 €</span></div></div><div class="progress"><span class="graph-barBack"><span class="graph-bar" data-value="10"></span></span></div></div></div></div>');
-  $("#events-show .habillage").prepend(
-    '<div class="stats-event mobile_flex-column"><div class="stats-event-2"><div class="stats-event-montants mobile_flex-column"><div class="stats-event-m-1"><span>97 560 €</span> collectés</div><div class="stats-event-m-2">Objectif: <span>100 000 €</span></div></div><div class="progress"><span class="graph-barBack"><span class="graph-bar" data-value="10"></span></span></div></div></div></div>'
-  );
-
-  // barre de progression
-  var pourcentage = $(".chart").attr("data-percent");
-  console.log(pourcentage);
-  $(".stats-event .graph-bar").attr("data-value", pourcentage);
-  $(".stats-event .graph-bar").css("width", pourcentage + "%");
-
   $(function () {
     if ($("body").is("#events-show") && !$("body").hasClass("event_5")) {
+      //ajout du bloc en début de page
+      // $('#events-show .habillage').prepend('<div class="stats-event mobile_flex-column"><div class="stats-event-1"><img src="https://agir.vivaforlife.be/cdn.iraiser.eu/6idKBeqjdjVcRtnL6A9qeBCllCI4cS9vXnl62IzT33g2GL6oGHWGvWlLOZdR4Xs7/Cecile_DEZY/origin/iconpeople2x.png" id="img-stat" alt=""><div class="stats-event-nb"><p>Déjà <span class="" style="font-size: 32px"><tag event_id='+ idCampaign +'></tag></span> collecteurs engagés</p></div></div><div class="line"></div><div class="stats-event-2"><div class="stats-event-montants mobile_flex-column"><div class="stats-event-m-1"><span>97 560 €</span> collectés</div><div class="stats-event-m-2">Objectif: <span>100 000 €</span></div></div><div class="progress"><span class="graph-barBack"><span class="graph-bar" data-value="10"></span></span></div></div></div></div>');
+      $("#events-show .habillage").prepend(
+        '<div class="stats-event mobile_flex-column"><div class="stats-event-2"><div class="stats-event-montants mobile_flex-column"><div class="stats-event-m-1"><span>97 560 €</span> collectés</div><div class="stats-event-m-2">Objectif: <span>100 000 €</span></div></div><div class="progress"><span class="graph-barBack"><span class="graph-bar" data-value="10"></span></span></div></div></div></div>'
+      );
+      // barre de progression
+      var pourcentage = $(".chart").attr("data-percent");
+      console.log(pourcentage);
+      $(".stats-event .graph-bar").attr("data-value", pourcentage);
+      $(".stats-event .graph-bar").css("width", pourcentage + "%");
       var collecte = $(".current-amount").html();
       $(".stats-event-m-1 span").html(collecte);
       console.log("collecte", collecte);
